00db1386d6b8ed41e86794f14fc58de2340810f9
USER <USER@DESKTOP-M0LT0IF>
2021-06-02 09:54:13 +0900

    0602 pageUtil update commit

--- moa/src/main/java/com/mono/moa/util/PageUtil.java
+++ moa/src/main/java/com/mono/moa/util/PageUtil.java
@@ -24,6 +24,10 @@ public class PageUtil {
 	}
 	
 	public PageUtil(int nowPage, int total, int pageRow, int pageGroup) {
+		setPage(nowPage, total, pageRow, pageGroup);
+	}
+
+	public void setPage(int nowPage, int total, int pageRow, int pageGroup) {
 		this.nowPage = nowPage;
 		this.total = total;
 		this.pageRow = pageRow;
@@ -33,7 +37,7 @@ public class PageUtil {
 		calcEndPage();
 		calcCont();
 	}
-
+	
 	public void calcPage() {
 		totalPage = (total % pageRow) == 0 ? ((total == 0) ? 1 : total / pageRow) : (total / pageRow + 1) ;
 	}
